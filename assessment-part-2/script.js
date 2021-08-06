02126aa77c7d43ca1344b95ece0c9fb51c244db7
Micah <micahstewart324@gmail.com>
2021-08-06 13:31:31 -0400

    I suck

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,26 @@
 // Insert Javascript Here
+//-- Cache references--//
+let counterMinus = document.querySelector('cnt-minus');
+let counterPlus = document.querySelector('cnt-plus');
+let num1 = document.getElementsByClassName('.num-inputs')
+
+
+//-- event listeners--//
+
+// counterPlus.addEventListener('click', num1)
+
+// function numPlus() {
+  
+//   return parseInt(num1.value) + parseInt(num1.value)
+  
+  // = parseInt(num1.value) - parseInt(num1.value)
+  //.value = mathResult
+  // output.innerText 
+// }
+
+// counterMinus.addEventListener("click", () => {
+//   return parseInt(num1.value) - parseInt(num1.value)
+// })
+function numPlus() {
+    return parseInt(num1.value) + parseInt(num1.value)
+  }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
+let strings = ["Hot","pocket"]
+strings.join()
 
 }
 
@@ -9,17 +11,30 @@ const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  let arrNums = [1,2,3,4]
+   let sum = 10 
+  for(i = 0; i < 4; i++){
+ return sum
+  }
+      
+    
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  let object = {
+    1: 'one', 
+    2: 'two', 
+    3: 'three',
+    4: 'four'
+  }
+    let result = ['one', 'two', 'three','four']
+    console.log(returnAllValues([1, 2, 3,4])); 
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return returnAllValues([1, 2, 3, 4])
 }
 
 const returnAllEvens = (nums) => {
