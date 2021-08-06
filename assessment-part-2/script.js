26745a5bc47b65988674647b395e66d542e60800
Sam Bassong <sam.bassong@gmail.com>
2021-08-06 11:45:22 -0700

    saving

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -9,17 +9,25 @@ input.value = 1
 
 
 plusBut.addEventListener('click', () => {
-  counter.innerText += input.value //I can't solve this issue where the input value is added to the counter value as a string
+  if (isNaN(input.value)) {
+    alert(`${input.value} is not a number, please try again`)
+  } else {
+    counter.innerText += parseInt(input.value) //I can't solve this issue where the input value is added to the counter value as a string
 
-  if (counter.innerText < 0) {
-  counter.style.color = 'red'
-}
+    if (counter.innerText < 0) {
+    counter.style.color = 'red'
+    }
+  }
 })
 
 minusBut.addEventListener('click', () => {
-  counter.innerText -= input.value
+  if (isNaN(input.value)) {
+    alert(`${input.value} is not a number, please try again`) 
+  } else {
+    counter.innerText -= parseInt(input.value)
 
-  if (counter.innerText < 0) {
-  counter.style.color = 'red'
-}
+    if (counter.innerText < 0) {
+    counter.style.color = 'red'
+    }
+  }
 })
