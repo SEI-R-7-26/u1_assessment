e218b14ecfdf7b9d8a7fba1ae98e5201829e1571
Janay Anguiano <janayanguiano@gmail.com>
2021-08-06 11:32:25 -0700

    assessment1

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,19 @@
-// Insert Javascript Here
+const negButton = document.querySelector('.negative-button')
+const posButton = document.querySelector('.positive-button')
+let input = input
+let value = value
+const displayWindow = (negButton.innerHTML = 'negative')
+negButton.addEventListener('click', function () {
+  if (negButton === true) {
+    let input = `${input}-`
+  }
+  console.log(displayWindow)
+})
+document.body.appendChild(negButton)
+{
+}
+posButton.innerHTML = 'positive'
+posButton.addEventListener('click', function () {
+  if (posButton === true) alert(`positive button is clicked`)
+})
+document.body.appendChild(posButton)
