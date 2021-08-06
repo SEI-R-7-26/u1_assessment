45ac6b2d929b228df86f4c56608159ef1b1f1507
Raza Khalid <razakhalid69@gmail.com>
2021-08-06 14:32:39 -0400

    assessment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,10 @@
-// Insert Javascript Here
+const addButton = document.querySelectorAll('#add')
+const subtractButton = document.querySelectorAll('#subtract')
+const input = document.querySelector('input')
+
+addButton.addEventListener('click', () => {
+  input.value = parseInt(input.value) - 1
+})
+addButton.addEventListener('click', () => {
+  input.value = parseInt(input.value) + 1
+})
--- index.js
+++ index.js
@@ -2,34 +2,52 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  fucntion(str1, str2)
+  let nums = str1 + str2
+  return nums
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  arrNums = 0
+  for (let i = 0; i >= 0; i++) {
+    if (arrNums[i] < 0) {
+      return arrNums
+    }
+  }
 }
 
-const returnAllValues = (object) => {
+const allValues = (object) => {
   // return all values from the provided object in a new array
-
+  function getValues (object, values)
+  let value = []
+  for (let i=0; i >= 0; i++)
+  return values; 
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return (countItems.arr.length)
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
+  function evens ()
+  let evenArray = []
+  for (let i = 0; i < a.length; i++) {
+    evenArray.push(a[2 * i + 1]);
+  }
 
+  return evenArray;
 }
+alert(even(returnAllEvens));
+
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  
 }
 
 module.exports = {
