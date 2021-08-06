a7b11bebe1c7dacfc539f54f0066ba8f9eb65c3f
Martha <marthamergia@yahoo.com>
2021-08-06 14:32:20 -0400

    assesment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,30 @@
 // Insert Javascript Here
+const buttonP = document.querySelector('.add-button')
+const buttonM = document.querySelector('.minus-button')
+const myInput = document.querySelector('.my-input')
+const myCounter = document.querySelector('.counter')
+
+buttonP.addEventListener('click', handleClick)
+buttonM.addEventListener('click', handleClickT)
+
+function handleClick(event) {
+  let myResult = parseInt(myInput.value)
+  let waga = myResult + 1
+  myCounter.innerHTML = waga
+  console.log(waga)
+}
+
+function handleClickT(event) {
+  let myResult = parseInt(myInput.value)
+  let mels = myResult - 1
+  myCounter.innerHTML = mels
+  console.log(mels)
+}
+
+buttonM.addEventListener('mouseover', handleHover)
+buttonP.addEventListener('mouseover', handleHover)
+
+function handleHover(event) {
+  buttonP.style.backgroundColor = 'pink'
+  buttonM.style.backgroundColor = 'pink'
+}
--- index.js
+++ index.js
@@ -2,35 +2,50 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+return str1 + ' ' + str2
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  let sum = 0;
+for (let i=0; i < arrNums.length; i++) {
+return sum += arrNums[i]
 }
+}
+
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+let newArray = Object.values(object)
+return newArray
 }
+
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
+
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  let answer = [];
+  for (let i=0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      answer.push(nums[i])
+    }
+    return answer
+  }
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+let result = math.pow(num, pow)
+  return result
 }
+
 
 module.exports = {
   joinStrings,
