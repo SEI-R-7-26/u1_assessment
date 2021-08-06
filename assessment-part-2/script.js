f910bcfadbbec513dcd05bc9b4f7203493957d9b
MistermindX <max.arenstein@gmail.com>
2021-08-06 12:14:30 -0400

    second assessment complete

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,20 @@
 // Insert Javascript Here
+const counter = document.querySelector(`.counter`)
+const plusButton = document.querySelector(`.plusButton`)
+const minusButton = document.querySelector(`.minusButton`)
+const number = document.getElementsByClassName(`number`)
+
+plusButton.addEventListener(`click`, function () {
+  counter.innerHTML = parseInt(counter.innerHTML, 10)+parseInt(number[0].value,10)
+  if (counter.innerHTML>=0){
+    counter.style.color = `black`
+  } 
+})
+
+minusButton.addEventListener(`click`, function () {
+  counter.innerHTML = parseInt(counter.innerHTML, 10)-parseInt(number[0].value,10)
+  if (counter.innerHTML<=0){
+    counter.style.color = `red`
+  } 
+  
+})
