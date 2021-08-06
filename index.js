5e85386bc8ffc616b9d6efd250b368c6c009c385
lfoster1150 <lfoster1150@gmail.com>
2021-08-06 09:29:56 -0500

    Finished part 1

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  const stringCombine = `${str1} ${str2}`
+  return stringCombine
 }
 
 const absoluteSum = (arrNums) => {
@@ -10,26 +11,40 @@ const absoluteSum = (arrNums) => {
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
 
+  return arrNums.reduce((acc, item) => {
+    if (typeof item === 'number') {
+      acc += item
+      return acc
+    } else {
+      return acc
+    }
+  }, 0)
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  return Object.values(object)
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  return nums.filter((num) => {
+    return num % 2 === 0
+  })
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  let product = num
+  for (let i = 1; i < pow; i++) {
+    product *= num
+  }
+  return product
 }
 
 module.exports = {
