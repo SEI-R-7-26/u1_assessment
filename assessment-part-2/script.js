e317adf8f4f32f2e1699be50ec989a6834503fe9
jnr0790 <jromero0790@gmaill.com>
2021-08-06 17:26:02 -0400

    Adds responses to u1_assessment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,18 @@
-// Insert Javascript Here
+let plus = document.getElementById('plus')
+let minus = document.getElementById('minus')
+
+let total = document.getElementById('total')
+let count = 0
+
+
+plus.addEventListener('click', event => {
+  let num = document.getElementById('num').value
+  console.log(count += num)
+  total.textContent = count
+})
+
+minus.addEventListener('click', event => {
+  let num = document.getElementById('num').value
+  count -= num
+  total.textContent = count
+})
--- index.js
+++ index.js
@@ -3,33 +3,55 @@
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
 
+  // create new variable for newString
+  // add str1 to space string to str2
+  const newString = str1 + ' ' + str2
+  return newString
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
 
+  let valArr = []
+
+  for (const val in object){
+    valArr.push(object[val])
+  }
+
+  return valArr
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
 
+  // creat a new array for evenNums
+  // filter the even numbers in the array
+  const evenNums = nums.filter(function(num) {
+    // if number is even return that number
+    if(num % 2 === 0) {
+      return num
+    }
+  })
+
+  return evenNums
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
 
+ return num ** pow
 }
 
 module.exports = {
