37bed42967a9a7e766b1c3a1a6adafe064c81bc7
Sam Bassong <sam.bassong@gmail.com>
2021-08-06 09:37:22 -0700

    commiting to new repo

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,25 @@
 // Insert Javascript Here
+const minusBut = document.querySelector('.decrement')
+const plusBut = document.querySelector('.increment')
+const counter = document.querySelector('.counter')
+let input = document.querySelector('input')
+
+counter.innerText = 0
+input.value = 1
+
+
+plusBut.addEventListener('click', () => {
+  counter.innerText += input.value //I can't solve this issue where the input value is added to the counter value as a string
+
+  if (counter.innerText < 0) {
+  counter.style.color = 'red'
+}
+})
+
+minusBut.addEventListener('click', () => {
+  counter.innerText -= input.value
+
+  if (counter.innerText < 0) {
+  counter.style.color = 'red'
+}
+})
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
+  return `${str1} ${str2}`
 
 }
 
@@ -9,26 +10,44 @@ const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
+  let sum = 0
+  for (let i = 0; i < arrNums.length; i++) {
+    if (typeof arrNums[i] !== 'number') break //return doesn't work here? why?
+      // sum += Math.abs(arrNums[i])
+      sum += arrNums[i]
+  }
+  return sum
 
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  let newArr = []
+  for (let key in object) {
+    newArr.push(object[key]) //Why does dot notation now work
+  }
+  return newArr
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  let count = 0
+  arr.forEach(element => {
+    count++
+  });
+  return count
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  return nums.filter((num) => {
+    if (num % 2 === 0) return num
+  })
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
+  return Math.pow(num, pow)
 
 }
 
