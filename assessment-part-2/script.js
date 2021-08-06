2a181bd0c91601c21226000ae466ddb1536bc0c9
msiroilem <twjones86@gmail.com>
2021-08-06 13:30:30 -0400

    assessment not finished

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,15 @@
-// Insert Javascript Here
+const plusButton = document.querySelector('#plus')
+const minusButton = document.querySelector('#minus')
+const inputField = document.querySelector('#number-input')
+let count = 0
+
+plusButton.addEventListener('click', addNumbers)
+minusButton.addEventListener('click', subtractNumbers)
+
+function addNumbers(num1, num2) {
+  count += inputField
+}
+
+function subtractNumbers(num1, num2) {
+  count -= inputField
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
 
+  return str1 + ' ' + str2
 }
 
 const absoluteSum = (arrNums) => {
@@ -10,26 +11,29 @@ const absoluteSum = (arrNums) => {
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
 
+  for (let i = 0; i < nums.length; i++) {}
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
+  let obj = { 1: 'one', 2: 'two', 3: 'three' }
 
+  return Object.values(object)
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  return num % 2 === 0
 }
-
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  return num ** pow
 }
 
 module.exports = {
