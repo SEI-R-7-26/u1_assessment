49f9979665edfd9baca830fcb5052bed84644c00
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-06 13:58:05 -0400

    Wei Assessment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,32 @@
 // Insert Javascript Here
+// variables
+let counter = document.getElementById('counter')
+
+let minus = document.getElementById('minus')
+
+let plus = document.getElementById('plus')
+
+let enteredNumber = document.getElementById('enter-number')
+
+let count = 0
+// functions
+function input() {
+  return parseInt(enteredNumber.value)
+}
+// event listeners
+plus.addEventListener('click', () => {
+  count += input()
+  counter.innerHTML = count
+  if (counter.innerHTML > 0) {
+    counter.style.color = 'black'
+  }
+})
+
+minus.addEventListener('click', () => {
+  count -= input()
+  counter.innerHTML = count
+  if (counter.innerHTML < 0) {
+    counter.style.color = 'red'
+  }
+})
+//
--- index.js
+++ index.js
@@ -2,34 +2,47 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  return `${str1} ${str2}`
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  let sum = 0
+  arrNums.forEach((num) => {
+    if (Number(num)) {
+      sum += num
+    }
+  })
+  return sum
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  let newArr = Object.values(object)
+  return newArr
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  let newArr = []
+  nums.forEach((num) => {
+    if (num % 2 === 0) {
+      newArr.push(num)
+    }
+  })
+  return newArr
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  return Math.pow(num, pow)
 }
 
 module.exports = {
