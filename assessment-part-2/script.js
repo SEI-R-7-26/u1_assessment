a80e8d5781ba540e6f018dc749064c0f2f880bd4
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-08-06 12:45:41 -0600

    assessment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,19 @@
 // Insert Javascript Here
+// console.log('hello world')
+let counter = document.querySelector('.counter')
+let plusButton = document.querySelector('.plus')
+let minusButton = document.querySelector('.minus')
+let input = document.querySelector('input')
+console.log(input.innerText)
+input.value = '1'
+
+plusButton.addEventListener('click', function () {
+  counter.innerText += input.value
+})
+
+minusButton.addEventListener('click', function () {
+  counter.innerText -= input.value
+  if (counter.innerHTML < 0) {
+    counter.style.color = 'red'
+  }
+})
