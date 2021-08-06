e24a1498f475ccf1251df94404db8dabcef4c3df
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-08-06 13:32:36 -0400

    finish

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,29 @@
 // Insert Javascript Here
+let input = document.querySelector(`input`)
+let value = parseInt(input.value)
+let plus = document.getElementById(`left`)
+let minus = document.getElementById(`right`)
+let number = document.querySelector(`h1`)
+let numberResult = parseInt(number.innerText)
+
+plus.addEventListener(`click`, function () {
+  let final = numberResult + value
+  number.innerText = final
+})
+
+minus.addEventListener(`click`, function () {
+  let final = numberResult - value
+  number.innerText = final
+})
+
+plus.addEventListener(`mouseenter`, function (event) {
+  event.target.style.backgroundColor = 'black'
+  event.target.style.color = 'gray'
+})
+
+minus.addEventListener(`mouseenter`, function (event) {
+  event.target.style.backgroundColor = 'black'
+  event.target.style.color = 'gray'
+})
+
+// I was able to get the initial value of the input to add or subtract from the counter. What I can't figure out is how to get the math to work with whatever updated values are in the input. I double checked and changed the initial values of the counter and input to make sure it was properly doing the math and it was. It seems to only care about the initial values of the elements.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  return str1 + ' ' + str2
 }
 
 const absoluteSum = (arrNums) => {
@@ -10,26 +10,42 @@ const absoluteSum = (arrNums) => {
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
 
+  let counter = 0
+  for (let i = 0; i < arrNums.length; i++) {
+    if (isNaN(arrNums[i] === false)) {
+      counter += arrNums[i]
+    } else {
+      return null
+    }
+  }
+  return counter
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  return Object.values(object)
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  let newArr = nums.filter((num) => {
+    if (num % 2 === 0) {
+      return num
+    } else {
+      return null
+    }
+  })
+  return newArr
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  return num ** pow
 }
 
 module.exports = {
