64f58abff8164d0f065b9c2549629820af066f0c
robertthome <remedy42290@gmail.com>
2021-08-06 09:27:04 -0700

    Almost

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,29 @@
 // Insert Javascript Here
+let topNumber = document.querySelector('#top-num')
+let add = document.querySelector('#add')
+let subtract = document.querySelector('#subtract')
+let addedNumber = document.querySelector('.up')
+let subtractNumber = document.querySelector('.down')
+let secondNum = document.querySelector('.secondNum')
+let inc = document.querySelector('.inc')
+let dec = document.querySelector('.dec')
+
+let counter = 0
+let bottomCounter = 1
+
+add.addEventListener('click', () => {
+  counter++
+  bottomCounter++
+  topNumber = counter
+  addedNumber.innerHTML = topNumber
+  secondNum = bottomCounter
+  inc.innerHTML = bottomCounter
+})
+
+subtract.addEventListener('click', () => {
+  counter--
+  bottomCounter--
+  topNumber = counter
+  subtractNumber.innerHTML = topNumber
+  dec.innerHTML = bottomCounter
+})
