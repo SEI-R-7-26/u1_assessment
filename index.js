899526dfd49bffe04c76d6f0aeab59c69f40adfb
Janay Anguiano <janayanguiano@gmail.com>
2021-08-06 11:48:56 -0700

    assessment1

--- index.js
+++ index.js
@@ -2,29 +2,42 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+let str1 = 'joe'
+let str2 = 'dirt'
 }
-
+{
+  return `${str1} + ${str2}`
+}
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+for(let i = 0; i < arrNums.length; i++)
+newArray = 
 }
+
+
+
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  newArray = object.length
+}
+{
+  return newArray
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+totalItems = arr.length + arr
 }
-
+{
+  return totalItems
+}
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  for(let i = 0; i < nums.length; i++)
+  evenNums = 
 }
 
 const returnPower = (num, pow) => {
