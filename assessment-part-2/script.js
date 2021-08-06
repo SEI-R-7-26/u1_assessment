24630640503c2fe87292b95d34cd28732696d13b
ItalloGama <Itallo.gama15@gmail.com>
2021-08-06 13:20:25 -0400

    test

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,20 @@
 // Insert Javascript Here
+let decreaseBtn = document.getElementById('buttonDecrease');
+let increaseBtn = document.getElementById('buttonIncrease');
+let counter = document.getElementById('counter');
+let input = document.getElementById('input')
+let count = 0
+
+input.addEventListener('click', () =>{
+    counter.innerText = input.value
+})
+
+decreaseBtn.addEventListener('click', () => {
+    count --;
+    counter.innerHTML = count;
+})
+
+increaseBtn.addEventListener('click', () => {
+    count ++;
+    counter.innerHTML = count;
+})
