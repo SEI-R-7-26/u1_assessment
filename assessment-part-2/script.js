d9ad362bf7daa9e2d51efff7a1c7b6ee9fcb0972
Sam <sam.k.phillips@gmail.com>
2021-08-06 12:34:23 -0400

    Assessment Complete

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,25 @@
-// Insert Javascript Here
+let count = 0
+
+const counterDisplay = document.querySelector(`#counter-display`)
+const counterInput = document.querySelector(`#counter-input`)
+const buttonPlus = document.querySelector(`#button-plus`)
+const buttonMinus = document.querySelector(`#button-minus`)
+
+const onButtonClicked = (e) => {
+  if (isNaN(counterInput.value)) {
+    alert(`${counterInput.value} is not a valid input. Numbers only, please.`)
+  } else {
+    count += counterInput.value * e.target.value
+
+    counterDisplay.innerHTML = `${count}`
+
+    if (count < 0) {
+      counterDisplay.style.color = `red`
+    } else {
+      counterDisplay.style.color = `black`
+    }
+  }
+}
+
+buttonPlus.addEventListener(`click`, onButtonClicked)
+buttonMinus.addEventListener(`click`, onButtonClicked)
--- index.js
+++ index.js
@@ -2,34 +2,56 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  return `${str1} ${str2}`
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
+  let sum = 0
 
+  arrNums.forEach((n) => {
+    if (Number.isInteger(n)) {
+      sum += n
+    }
+  })
+
+  return sum
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
+  let oArr = []
 
+  Object.keys(object).forEach((key) => {
+    oArr.push(object[key])
+  })
+
+  return oArr
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
+  let oArr = []
 
+  nums.forEach((n) => {
+    if (n % 2 === 0) {
+      oArr.push(n)
+    }
+  })
+
+  return oArr
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  return Math.pow(num, pow)
 }
 
 module.exports = {
