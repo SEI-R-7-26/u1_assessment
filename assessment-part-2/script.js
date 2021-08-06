6ee1b153df5e3a5dd6e4c6be01dddb9ea65d4c74
Sam'an <samangriffiths47@gmail.com>
2021-08-06 12:06:09 -0500

    finit

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,20 @@
 // Insert Javascript Here
+let input = document.getElementById(`input`)
+let plus = document.querySelector(`.plus`)
+let minus = document.querySelector(`.minus`)
+
+plus.addEventListener(`click`, function () {
+  let solution = document.querySelector(`header`)
+  if (Number.isNaN(parseFloat(input.value)) === false) {
+    let sum = parseFloat(solution.innerHTML) + parseFloat(input.value)
+    solution.innerHTML = `${sum}`
+  }
+})
+
+minus.addEventListener(`click`, function () {
+  let solution = document.querySelector(`header`)
+  if (Number.isNaN(parseFloat(input.value)) === false) {
+    let sum = parseFloat(solution.innerHTML) - parseFloat(input.value)
+    solution.innerHTML = `${sum}`
+  }
+})
