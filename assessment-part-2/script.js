19c4958ffc6a1a5011f109db3d1f5cbd7d3c8404
rcraig <ryanpcraig1@gmail.com>
2021-08-08 16:53:43 -0500

    Finished Assessment

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,43 @@
 // Insert Javascript Here
+const inc_button = document.getElementById("increment")
+const dec_button = document.getElementById("decrement")
+const num_input = document.querySelector(".inc")
+let count = 0
+const count_dis = document.querySelector(".count")
+
+inc_button.addEventListener('click', incCount)
+inc_button.addEventListener('mouseover', revColI)
+inc_button.addEventListener('mouseout', retColI)
+dec_button.addEventListener('click', decCount)
+dec_button.addEventListener('mouseover', revColD)
+dec_button.addEventListener('mouseout', retColD)
+
+function revColI() {
+  inc_button.style.color ="grey";
+  inc_button.style.backgroundColor ="white";
+}
+
+function retColI() {
+  inc_button.style.color ="white";
+  inc_button.style.backgroundColor ="grey";
+}
+
+function revColD() {
+  dec_button.style.color ="grey";
+  dec_button.style.backgroundColor ="white";
+}
+
+function retColD() {
+  dec_button.style.color ="white";
+  dec_button.style.backgroundColor ="grey";
+}
+
+function incCount() {
+  count += parseInt(num_input.value)
+  count_dis.innerHTML = count
+}
+
+function decCount() {
+  count -= parseInt(num_input.value)
+  count_dis.innerHTML = count
+}
--- index.js
+++ index.js
@@ -2,34 +2,55 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  return str1 + " " + str2
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  let sum = 0
+  for (i = 0; i < arrNums.length; i++) {
+    if (typeof(arrNums[i]) === "number") {
+      sum += arrNums[i]
+    }
+  }
+  return sum
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  let newArr = []
+  let values = Object.values(object)
+  for (i = 0; i < values.length; i++) {
+    newArr.push(values[i])
+  }
+  return newArr
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  let count = 0
+  for (i = 0; i < arr.length; i++) {
+    count += 1
+  }
+  return count
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  let evens = []
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0) {
+      evens.push(nums[i])
+    }
+  }
+  return evens
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  return Math.pow(num, pow)
 }
 
 module.exports = {
