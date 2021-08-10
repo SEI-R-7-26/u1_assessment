e25807f657d21a056c9a96d86a878919c23d894f
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-08-10 14:10:55 -0600

    resubmit

--- index.js
+++ index.js
@@ -2,34 +2,36 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  str1 = 'Hello'
+  str2 = 'World'
+  return `${str1} ${str2}`
 }
 
-const absoluteSum = (arrNums) => {
-  // Return the absolute sum of all INTEGERS in an array
-  // Note not all items in the array may be a number datatype
-  // Remember to check the type of the element
-
-}
+const absoluteSum = (arrNums) => {}
+// Return the absolute sum of all INTEGERS in an array
+// Note not all items in the array may be a number datatype
+// Remember to check the type of the element
 
 const returnAllValues = (object) => {
-  // return all values from the provided object in a new array
-
+  return Object.values(object)
 }
 
 const countItems = (arr) => {
+  return arr.length
   // return total number of items in the provided array
-  
 }
 
 const returnAllEvens = (nums) => {
+  const evens = nums.filter((num) => {
+    return (num = num % 2 === 0)
+  })
+  return evens
   // return all even numbers in a new array from the provided nums array.
-
 }
 
 const returnPower = (num, pow) => {
+  return (num = Math.pow + pow)
   // return the provided num to the power provided
-
 }
 
 module.exports = {
