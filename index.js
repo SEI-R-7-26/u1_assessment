004662bcd4500b83b0a459da9f6cef04d815dddb
MICHAEL <MJENNINGS6@gmail.com>
2021-08-20 16:04:34 -0400

    part 1

--- index.js
+++ index.js
@@ -2,35 +2,79 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
+  let combine = str1 + " " + str2;
 
-}
+  return combine;
+};
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
+  // arrNums.forEach((number) => {
+  let sum = 0;
+  for (let i = 0; i < arrNums.length; i++) {
+    // console.log(typeof arrNums[i]);
+    let res = (sum += arrNums[i]);
+    if (typeof arrNums[i] !== "number") {
+      arrNums.splice([i]);
+      console.log("d:", arrNums);
+      let arr = arrNums;
+      console.log("New array:", arr);
+      return arr;
+    }
+    console.log(res);
+    console.log(sum);
 
-}
+    // if (arrNums[i] < 0) {
+    //   // console.log(arrNums[i]);
+    //   arrNums.filter[i];
+    //   console.log(arrNums.filter[i]);
+    //   // let arr = [];
+    //   // arr.push(arrNums);
+    //   // console.log("arr:", arr);
+
+    //   // console.log(arrNums);
+    //   // return arr;
+    //   // console.log((sum += arrNums[i]));
+    // }
+  }
+};
+//   if (typeof number === "number") {
+//     console.log("New number:", number);
+//   }
+//   console.log(("Sum of Numbers:", (sum += number[i])));
+// });
+// return "Sum:", sum;
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
-}
+};
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
-}
+  for (let i = 0; i < arr.length; i++) {
+    console.log("count:", arr.length);
+    return arr.length;
+  }
+};
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
-}
+  for (let i = 0; i < nums.length; i++) {
+    console.log("AE:", nums);
+    if (nums[i] % 2 === 0) {
+      return nums;
+    }
+  }
+};
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
-}
+  ans = num ** pow;
+  // console.log("power:", num ** pow);
+  return ans;
+};
 
 module.exports = {
   joinStrings,
@@ -38,5 +82,5 @@ module.exports = {
   returnAllValues,
   countItems,
   returnAllEvens,
-  returnPower
-}
+  returnPower,
+};
